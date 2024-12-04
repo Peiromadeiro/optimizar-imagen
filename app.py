1a284c0ee5ffab1f2d13c12d4b42e526af3317b8
Peiromadeiro <86718443+Peiromadeiro@users.noreply.github.com>
2024-12-04 12:44:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -67,4 +67,7 @@ def optimize_image():
         return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
